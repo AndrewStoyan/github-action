ba4d138d6be92eb73ae24ab03c0baf982cd07a0e
Andrew Stoyan <AndrewStoyan@users.noreply.github.com>
2021-03-18 19:37:16 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ init_options() {
   fi
 
   if [ -n "$INPUT_CROWDIN_BRANCH_NAME" ]; then
-    OPTIONS="${OPTIONS} --branch=\"${INPUT_CROWDIN_BRANCH_NAME}\""
+    OPTIONS="${OPTIONS} --branch=${INPUT_CROWDIN_BRANCH_NAME}"
   fi
 
   if [ -n "$INPUT_IDENTITY" ]; then
